0e1637c9ca9637853509e5ca4315b34bde22f051
nazim-influencer <mohammad.nazim@influencer.in>
2026-03-07 23:31:44 +0530

    abcd

--- institute_api/settings.py
+++ institute_api/settings.py
@@ -89,7 +89,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://3.110.173.121:5173"
+    "http://3.110.173.121:5173",
+    "https://main.d3e0ptm41k0biu.amplifyapp.com"
 ]
 
 from corsheaders.defaults import default_headers
